d0300b80db73f51462460e03c6e55c527f2bebeb
JiahaiWu <jiahai.wu@foxmail.com>
2015-05-14 14:36:08 +0800

    add version

--- IDCMPro/Properties/AssemblyInfo.cs
+++ IDCMPro/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("IDCMPro")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GCMLite")]
+[assembly: AssemblyDescription("Specific Data Admin For GCM Data Intergration")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("IDCMPro")]
+[assembly: AssemblyCompany("NIC IMCAS")]
+[assembly: AssemblyProduct("GCMLite")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
